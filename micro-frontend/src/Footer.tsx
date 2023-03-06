1ee23e5c29f8aa571da9f63affe84cfb02438c30
mohammad_saadati <saadati.m.b@gmail.com>
2023-03-06 17:38:58 +0330

    test lazy loading and suspense

--- micro-frontend/src/Footer.tsx
+++ micro-frontend/src/Footer.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="">footer from microfrontend</div>;
+  return (
+    <>
+      <div className="">footer from microfrontend</div>
+      <div>Footer changed</div>
+    </>
+  );
 }
--- pdp/src/App.tsx
+++ pdp/src/App.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-import Footer from "micro_frontend/Footer";
-import Header from "micro_frontend/Header";
+// import Footer from "micro_frontend/Footer";
+const Footer = React.lazy(() => import("micro_frontend/Footer"));
+// import Header from "micro_frontend/Header";
+const Header = React.lazy(() => {
+  return new Promise((resolve, reject) =>
+    setTimeout(
+      () =>
+        1 ? resolve(import("micro_frontend/Header")) : reject("sth went wrong"),
+      5000
+    )
+  );
+});
 
 import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <div>Name: pdp</div>
-    <Header />
-    <Footer />
+    <Suspense fallback={<div>loading ...</div>}>
+      <Header />
+      <Footer />
+    </Suspense>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
